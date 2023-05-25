b16a3473a5c49450ccf4dde2ead963b4ee791c57
TxemaBlanco <sam@example.comtxemabl@hotmail.com>
2023-05-25 10:14:09 +0200

    nueva1

--- ex-1/app.js
+++
@@ -1,9 +0,0 @@
-/*
-* Conectar archivo javascript al html de destino
-* Imprimir Hello Javascript! en nuestra ventana del navegador
-* * definir el contenedor o espacio que va a ocupar el mensaje Hello JS
-* * acceder al contenedor desde javascript
-* * crear el mensaje
-* * insertar en el contenedor
-* * imprimirlo en pantalla
-*/
